e4f36e1f9242b01c54aadb2b93903d742adcc17c
Davide Dal Bosco <dalbosco92@hotmail.it>
2026-06-07 12:16:35 +0200

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    // Incolla qui il link della Web App fornito da Google Apps Script dopo il deployment
-    googleScriptURL: "INSERISCI_QUI_IL_TUO_URL_DI_GOOGLE_SCRIPT",
+    // URL definitivo di Google Apps Script fornito dall'utente
+    googleScriptURL: "https://script.google.com/macros/s/AKfycbyYG9TV_rk-DHbBLI-rbmq4emxyRFobM_AnJFjNScYb2PydPRD78w04LySqYQtiLFrWdA/exec",
     
     // Total Iodine Load (TIL) in mgI/kg per ciascun distretto Body
     tilParametri: {
@@ -11,17 +11,17 @@ const CONFIG = {
         "COLLO": 430
     },
 
-    // Sconti moltiplicativi per le tecnologie TC (es. 0.95 = sconto del 5%)
+    // Sconti moltiplicativi per le tecnologie TC (es. 0.85 = sconto del 15%)
     scontiTecnologici: {
-        "SECT_Iterative": 1.0,       // Baseline standard (nessuno sconto)
-        "SECT_DeepLearning": 0.95,   // Sconto 5%
-        "DECT_Iterative": 0.90,      // Sconto 10%
-        "DECT_DeepLearning": 0.85    // Sconto 15% (Massima efficienza combi)
+        "SECT_Iterative": 1.0,       
+        "SECT_DeepLearning": 0.95,   
+        "DECT_Iterative": 0.90,      
+        "DECT_DeepLearning": 0.85    
     },
 
     // Correttivi in millilitri (ml) basati sulla Frequenza Cardiaca (FC)
     correzioneFrequenza: {
-        sotto60: -7,   // ml da sottrarre se FC <= 60 BPM
-        sopra80: 5     // ml da aggiungere se FC >= 80 BPM
+        sotto60: -7,   
+        sopra80: 5     
     }
 };

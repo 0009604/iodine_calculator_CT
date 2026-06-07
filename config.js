e9bad39930a193bea0147c88082cb68ee46236e3
Davide Dal Bosco <dalbosco92@hotmail.it>
2026-06-07 21:22:08 +0200

    config.js

--- config.js
+++ config.js
@@ -1,53 +1,25 @@
 const CONFIG = {
-    // URL definitivo di Google Apps Script per la sincronizzazione cloud
     googleScriptURL: "https://script.google.com/macros/s/AKfycbyYG9TV_rk-DHbBLI-rbmq4emxyRFobM_AnJFjNScYb2PydPRD78w04LySqYQtiLFrWdA/exec",
     
-    // Configurazione studi BODY: Total Iodine Load (TIL) in mgI/kg
     tilParametri: {
-        "STADIAZIONE": 720,
-        "TORACE": 450,
-        "ADDOME": 670,
-        "CRANIO": 400,
-        "COLLO": 430
+        "STADIAZIONE": 720, "TORACE": 450, "ADDOME": 670, "CRANIO": 400, "COLLO": 430
     },
 
-    // Sconti moltiplicativi per le tecnologie TC studi BODY
     scontiTecnologici: {
-        "SECT_Iterative": 1.0,       
-        "SECT_DeepLearning": 0.95,   
-        "DECT_Iterative": 0.90,      
-        "DECT_DeepLearning": 0.85    
+        "SECT_Iterative": 1.0, "SECT_DeepLearning": 0.95, "DECT_Iterative": 0.90, "DECT_DeepLearning": 0.85    
     },
 
-    // Correttivi volumetrici fissi in ml basati sulla Frequenza Cardiaca (BODY)
-    correzioneFrequenza: {
-        sotto60: -7,   
-        sopra80: 5     
-    },
+    correzioneFrequenza: { sotto60: -7, sopra80: 5 },
 
-    // Configurazione studi vascolari CTA
+    // CTA più conservativa: volumi leggermente ridotti
     ctaParametri: {
-        "Willis/TSA":         { idrTarget: 1.3, rangeOk: [1.1, 1.6], baseVol: 45,  floor: 25 },
-        "Polmonare":          { idrTarget: 1.4, rangeOk: [1.2, 1.7], baseVol: 50,  floor: 30 },
-        "Aorta toracica":     { idrTarget: 1.8, rangeOk: [1.5, 2.2], baseVol: 90,  floor: 60 },
-        "Aorta toraco-add.":  { idrTarget: 1.8, rangeOk: [1.5, 2.2], baseVol: 100, floor: 65 },
-        "Arti inferiori":     { idrTarget: 1.6, rangeOk: [1.3, 2.0], baseVol: 110, floor: 70 }
+        "Willis/TSA":         { idrTarget: 1.2, rangeOk: [1.0, 1.5], baseVol: 42,  floor: 20 },
+        "Polmonare":          { idrTarget: 1.3, rangeOk: [1.1, 1.6], baseVol: 45,  floor: 25 },
+        "Aorta toracica":     { idrTarget: 1.6, rangeOk: [1.4, 2.0], baseVol: 80,  floor: 50 },
+        "Aorta toraco-add.":  { idrTarget: 1.6, rangeOk: [1.4, 2.0], baseVol: 90,  floor: 55 },
+        "Arti inferiori":     { idrTarget: 1.5, rangeOk: [1.2, 1.8], baseVol: 100, floor: 60 }
     },
 
-    // Sconti progressivi e conservativi per Angio-TC (CTA)
-    scontiCtaSECT: {
-        120: 0.00,
-        100: 0.08,
-        80:  0.20,
-        70:  0.32
-    },
-
-    // Esteso il range fino a 140 keV per ricostruzione DECT
-    scontiCtaDECT: {
-        140: 0.00, 135: 0.00, 130: 0.00, 125: 0.00, 120: 0.00, 
-        115: 0.00, 110: 0.00, 105: 0.00, 100: 0.00,
-        95:  0.025, 90: 0.05, 85: 0.075, 80: 0.10, 75: 0.125,
-        70:  0.15, 65: 0.185, 60: 0.22, 55: 0.26, 50: 0.30,
-        45:  0.34, 40: 0.38
-    }
+    scontiCtaSECT: { 120: 0.00, 100: 0.08, 80: 0.20, 70: 0.32 },
+    scontiCtaDECT: { 140: 0.00, 130: 0.00, 120: 0.00, 100: 0.00, 70: 0.15, 50: 0.30, 40: 0.38 }
 };
